9e7e79712b25aebc31fdf2b58b111603fca6bf72
harshit <harshitshrivastava340@gmail.com>
2024-04-23 21:48:42 +0530

    hello

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://rrqgnzvd:PtejfFfcq98MtkSVIpcdj_0KX6FjgTQP@tai.db.elephantsql.com/rrqgnzvd");
     await client.connect();
     return client;
 }
